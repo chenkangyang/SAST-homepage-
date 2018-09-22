a5c12b0d86b8b987e666064118b5885a2464ecb5
alex <13605184016@163.com>
2018-09-22 22:16:30 +0800

    修改dist目录的引用

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: './dist/',
     filename: '[name].bundle.js'
   },
   module: {
